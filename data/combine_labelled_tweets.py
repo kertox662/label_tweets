b978654b8183445d05d96a9d2848611ebbef953e
Mitchell <mitchelldolny@gmail.com>
2025-07-12 20:56:37 -0400

    Made master files for success fail and simple id file

--- data/combine_labelled_tweets.py
+++ data/combine_labelled_tweets.py
@@ -3,26 +3,29 @@ import os
 if __name__ == "__main__":
     path = 'tweets/Labelled_Tweets/'
 
-    dfs = []
-    for file in os.listdir(path):
-        if 'Done' in file:
-            full_path = os.path.join(path,file)
-            df = pd.read_csv(full_path)
-            df = df[['tweet_id', 'text', 'author_id', 'tw_date', 'year', 'AR', 'MB']]
-            df['tweet_id'] = df['tweet_id'].astype('Int64')
-            dfs.append(df)
+    success = pd.read_csv(path + '/success_new.csv')
+    failed = pd.read_csv(path + '/failed_new.csv')
 
-    data = pd.concat(dfs, ignore_index=True)
+    success['tweet_id'] = success['tweet_id_y']
+    success['old_tweet_id'] = success['tweet_id_x']
+    success['author_id'] = success['author_id_y']
+    success['old_author_id'] = success['author_id_x']
+    success['tw_date'] = success['tw_date_y']
+    success['text'] = success['text_y']
+    success['old_text'] = success['text_x']
 
-    other_data_2 = pd.read_csv(path + '/success.csv')
-    other_data_2['tweet_id'] = other_data_2['tweet_id_y'].astype('Int64')
-    other_data_2['tw_date'] = other_data_2['tw_date_y']
-    other_data_2['text'] = other_data_2['text_y']
-    other_data_2 = other_data_2.dropna(subset=['AR'])
+    failed['tweet_id'] = failed['tweet_id_y']
+    failed['old_tweet_id'] = failed['tweet_id_x']
+    failed['author_id'] = failed['author_id_y']
+    failed['old_author_id'] = failed['author_id_x']
+    failed['tw_date'] = failed['tw_date_y']
+    failed['text'] = failed['text_y']
+    failed['old_text'] = failed['text_x']
 
-    df_2017_20 = other_data_2[['tweet_id', 'text', 'author_id', 'tw_date', 'year', 'AR', 'MB']]
+    success_final = success[['tweet_id', 'old_tweet_id', 'text', 'old_text', 'author_id', 'old_author_id', 'tw_date', 'year', 'AR', 'MB', 'similarity']]
+    success_final.to_csv('success_master.csv', index=False)
 
-    final_df = pd.concat([data, df_2017_20], ignore_index=True)
-    final_df = final_df[['tweet_id', 'year', 'AR', 'MB']]
-    final_df['AR'] = final_df['AR'].astype('Int64')
+    failed_final = failed[['tweet_id', 'old_tweet_id', 'text', 'old_text', 'author_id', 'old_author_id', 'tw_date', 'year', 'AR', 'MB', 'similarity']]
+    failed_final.to_csv('failure_master.csv', index=False)
+    final_df = success_final[['tweet_id', 'year', 'AR', 'MB']]
     final_df.to_csv('final_combined_labelled.csv', index=False)
