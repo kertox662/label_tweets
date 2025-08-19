42fdd58b68cebad3da4a688b8b005c55431eb657
Alexander Tjhin <atjhin@narval2.narval.calcul.quebec>
2025-08-18 20:46:34 -0400

    supervised learning changes

--- jobscript.sh
+++ jobscript.sh
@@ -3,11 +3,13 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=0-12:00
+#SBATCH --time=0-8:00
+#SBATCH --mem=32G
 #SBATCH --job-name=test_2
 #SBATCH --output=logs/%x-%A-%a.out  # Optional: output log per array job/task
 
-module load python
+module load python StdEnv/2023 intel/2023.2.1 cuda/11.8 scipy-stack/2023b
+module load gcc arrow/19.0.1
 source ~/projects/def-jhoey/atjhin/label_tweets/venv/bin/activate
 
 python -m Refactored.main ${SLURM_ARRAY_TASK_ID}
